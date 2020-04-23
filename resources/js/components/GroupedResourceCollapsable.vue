2af968e75fb6eedd0a2527436d28952b901f5840
Ethan Brace <ethanabrace@gmail.com>
2020-04-23 09:50:13 -0400

    icon to start of folder name

--- resources/js/components/GroupedResourceCollapsable.vue
+++ resources/js/components/GroupedResourceCollapsable.vue
@@ -3,9 +3,12 @@
         <h4 ref="nova-category"
             class="leading-wide font-bold text-white-50% mb-2 text-sm capitalize cursor-pointer hover:text-white"
             @click.prevent="toggle">
-            {{ header }}
+
             <template v-if="expanded">&minus;</template>
             <template v-else>&plus;</template>
+<!---->
+            {{ header }}
+
         </h4>
 
         <slot v-if="expanded"></slot>
